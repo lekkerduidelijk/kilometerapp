0c8940cdf522522fdc582a83628629276874fc06
Rutger Laurman <lekkerduidelijk@gmail.com>
2013-01-11 23:34:40 +0100

    Creating Rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kilometerapp::Application.config.secret_token = '6e1a2e5c514ea3a192cde4f3ad806bb88f7bb7963e4c1f1b26c636c4ce35652da7503c6100ebcbe3572e44ff48a8785ca1fa658842a68855b7ff0b5a3c8c1d46'
